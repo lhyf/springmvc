30ecd4bb8b6926b241ad3c2990db6f5c4963ca0c
lhyf <lhyf@outlook.com>
2019-04-08 20:30:01 +0800

    。。

--- springmvc-3/src/main/java/org/lhyf/controller/IndexController.java
+++ springmvc-3/src/main/java/org/lhyf/controller/IndexController.java
@@ -3,6 +3,7 @@ package org.lhyf.controller;
 import org.lhyf.service.LoginService;
 import org.lhyf.util.DeferredResultQueue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
